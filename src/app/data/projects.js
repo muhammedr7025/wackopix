f8d27e83d82333c64d3e2644161443591b26b2da
muhammedr7025 <muhammedr7025@gmail.com>
2024-08-24 05:02:40 +0530

    placement changed

--- src/app/data/projects.js
+++ src/app/data/projects.js
@@ -5,7 +5,7 @@ export const projects = [
     img: dummy,
     title: "Project Name",
     desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.",
-    youtube: "xvFZjo5PgG0",
+    youtube: "GMwBuFRBZT8",
   },
   {
     img: dummy,
